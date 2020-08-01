af2faec3b8987c099f3423a71a52a1657577a804
Nicola Ferracin <nicola.ferracin@gmail.com>
2020-08-01 13:03:49 +0100

    Frontend: add page for editing single payment methods

---
+++ frontend/pages/payment-methods/[id].js
@@ -0,0 +1,40 @@
+import { useRouter } from 'next/router';
+import CategoryPaymentMethodForm from '../../componets/CategoryPaymentMethodForm';
+import SpendingContext from '../../componets/context';
+
+const EditPaymentMethod = () => {
+  const router = useRouter();
+  const { id } = router.query;
+
+  const handleSubmit = async (cookie, newName) => {
+    // TODO abstract all this config away
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/payment-methods/${id}`, {
+      method: 'PUT',
+      credentials: 'include',
+      headers: {
+        cookie,
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ name: newName })
+    });
+    return { status: res.status };
+  };
+
+  return (
+    <SpendingContext.Consumer>
+      {ctx => (
+        <>
+          <h1>Edit Payment Method</h1>
+          <CategoryPaymentMethodForm
+            page="payment method"
+            handleSubmit={handleSubmit}
+            name={ctx.paymentMethods.find(c => c._id === id).name}
+          />
+        </>
+      )}
+    </SpendingContext.Consumer>
+  );
+};
+
+export default EditPaymentMethod;
