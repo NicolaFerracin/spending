7fb37d6ad0579f00976369cefa73b196b1466553
Nicola Ferracin <nicola.ferracin@gmail.com>
2020-08-01 12:17:07 +0100

    Frontend: fetch entries, categories and paymentMethods on app load

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -4,33 +4,51 @@ import Login from '../componets/Login';
 import '../styles/globals.css';
 
 export default class MyApp extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      user: props.user
-    };
-  }
-
   static async getInitialProps({ ctx }) {
+    // TODO 1 refactor for parallel requests
+    // TODO 2 fix issue with losing credentials when changing route
     const initialState = {};
-    const res = await fetch(`${process.env.BACKEND_URL}/private`, {
-      headers: { cookie: ctx?.req?.headers?.cookie }
-    });
+    const cookie = ctx?.req?.headers?.cookie;
+    const fetchOpts = {
+      headers: { cookie }
+    };
+    const profileRes = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/profile`, fetchOpts);
 
-    if (res.status === 200) {
-      initialState.user = true;
+    if (profileRes.status === 200) {
+      const { _id, email, name } = (await profileRes.json()).user;
+      initialState.user = { _id, email, name };
+      initialState.cookie = cookie;
+
+      const entriesRes = await fetch(
+        `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/entries`,
+        fetchOpts
+      );
+      const entries = (await entriesRes.json()).entries;
+      initialState.entries = entries;
+
+      const categoriesRes = await fetch(
+        `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/categories`,
+        fetchOpts
+      );
+      const categories = (await categoriesRes.json()).categories;
+      initialState.categories = categories;
+
+      const paymentMethodsRes = await fetch(
+        `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/payment-methods`,
+        fetchOpts
+      );
+      const paymentMethods = (await paymentMethodsRes.json()).paymentMethods;
+      initialState.paymentMethods = paymentMethods;
     }
 
     return initialState;
   }
 
   render() {
-    const { Component, pageProps } = this.props;
-    const { user } = this.state;
+    const { Component, pageProps, user, cookie, entries, categories, paymentMethods } = this.props;
 
     return (
-      <SpendingContext.Provider value={{ user }}>
+      <SpendingContext.Provider value={{ user, cookie, entries, categories, paymentMethods }}>
         {user ? <Component {...pageProps} /> : <Login />}
       </SpendingContext.Provider>
     );
