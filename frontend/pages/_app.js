76cf8f4d710dd977f2977be252b92c9c5ca2e6a8
Nicola Ferracin <nicola.ferracin@gmail.com>
2020-08-02 19:26:03 +0100

    Frontend: remove fetching logic from app and wrap with AuthProvider

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,41 +1,15 @@
 import { Component } from 'react';
-import SpendingContext from '../componets/context';
-import Login from '../componets/Login';
-import ApiHandler from '../apiHandler';
+import { AuthProvider } from '../authContext';
 import '../styles/globals.css';
 
 export default class MyApp extends Component {
-  static async getInitialProps({ ctx }) {
-    const cookie = ctx?.req?.headers?.cookie;
-    const apiHandler = new ApiHandler(cookie);
-    apiHandler.get('something');
-    const initialState = { apiHandler };
-
-    const profileRes = await apiHandler.get('profile');
-    if (profileRes.status === 200) {
-      const { _id, email, name } = (await profileRes.json()).user;
-      initialState.user = { _id, email, name };
-      initialState.cookie = cookie;
-
-      const paths = ['api/entries', 'api/categories', 'api/payment-methods'];
-      return await Promise.all(
-        paths.map(path => apiHandler.get(path).then(res => res.json()))
-      ).then(([{ entries }, { categories }, { paymentMethods }]) => ({
-        ...initialState,
-        entries,
-        categories,
-        paymentMethods
-      }));
-    }
-  }
-
   render() {
-    const { Component, pageProps, user, cookie, entries, categories, paymentMethods } = this.props;
+    const { Component, pageProps } = this.props;
 
     return (
-      <SpendingContext.Provider value={{ user, cookie, entries, categories, paymentMethods }}>
-        {user ? <Component {...pageProps} /> : <Login />}
-      </SpendingContext.Provider>
+      <AuthProvider>
+        <Component {...pageProps} />
+      </AuthProvider>
     );
   }
 }
