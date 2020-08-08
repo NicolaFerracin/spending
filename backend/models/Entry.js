05d0c5534f03c8e38678c2fbbbe0cd15a794fc77
Nicola Ferracin <nicola.ferracin@gmail.com>
2020-08-08 20:53:59 +0100

    Backend: add day, month and year to Entry model to make it easier to work with it

--- backend/models/Entry.js
+++ backend/models/Entry.js
@@ -7,7 +7,10 @@ const EntrySchema = new Schema({
   paymentMethod: { type: Schema.Types.ObjectId, ref: 'PaymentMethod' },
   category: { type: Schema.Types.ObjectId, ref: 'Category' },
   date: { type: Date, default: Date.now() },
-  user: { type: Schema.Types.ObjectId, ref: 'User' }
+  user: { type: Schema.Types.ObjectId, ref: 'User' },
+  day: Number,
+  month: Number,
+  year: Number
 });
 
 module.exports = mongoose.model('Entry', EntrySchema);
