49a6407e99a2ce1b6582a17ee7d19d7015179456
Nicola Ferracin <nicola.ferracin@gmail.com>
2020-08-08 22:44:00 +0100

    Backend: use a catch-all error handler to avoid long loading time for failing requests

--- backend/routes.js
+++ backend/routes.js
@@ -9,6 +9,11 @@ const PaymentMethod = require('./models/PaymentMethod');
 
 const jwtMiddleware = passport.authenticate('jwt', { session: false });
 
+const catchErrors = fn => (req, res, next) =>
+  fn(req, res, next).catch(() => {
+    next('Uh Oh! Something went wrong.');
+  });
+
 router.get('/auth', passport.authenticate('google', { scope: ['profile', 'email'] }));
 
 router.get('/auth/callback', passport.authenticate('google'), (req, res) => {
@@ -23,147 +28,216 @@ router.get('/profile', jwtMiddleware, (req, res) => {
 });
 
 // get all
-router.get('/api/entries', jwtMiddleware, async (req, res) => {
-  const entries = await Entry.find({ user: req.user })
-    .sort({ date: 'desc' })
-    .populate('paymentMethod')
-    .populate('category');
-  res.json({ entries });
-});
-router.get('/api/categories', jwtMiddleware, async (req, res) => {
-  const categories = await Category.find({ user: req.user }).sort({ name: 'asc' });
-  res.json({ categories });
-});
-router.get('/api/payment-methods', jwtMiddleware, async (req, res) => {
-  const paymentMethods = await PaymentMethod.find({ user: req.user }).sort({ name: 'asc' });
-  res.json({ paymentMethods });
-});
+router.get(
+  '/api/entries',
+  jwtMiddleware,
+  catchErrors(async (req, res) => {
+    const entries = await Entry.find({ user: req.user })
+      .sort({ date: 'desc' })
+      .populate('paymentMethod')
+      .populate('category');
+    res.json({ entries });
+  })
+);
+router.get(
+  '/api/categories',
+  jwtMiddleware,
+  catchErrors(async (req, res) => {
+    const categories = await Category.find({ user: req.user }).sort({ name: 'asc' });
+    res.json({ categories });
+  })
+);
+router.get(
+  '/api/payment-methods',
+  jwtMiddleware,
+  catchErrors(async (req, res) => {
+    const paymentMethods = await PaymentMethod.find({ user: req.user }).sort({ name: 'asc' });
+    res.json({ paymentMethods });
+  })
+);
 
 // get for year and month
-router.get('/api/entries/:year/:month', jwtMiddleware, async (req, res) => {
-  const { year, month } = req.params;
-  const entries = await Entry.aggregate([
-    { $match: { user: mongoose.Types.ObjectId(req.user._id) } },
-    { $match: { year: Number(year), month: Number(month) } }
-  ]).sort({ date: 'desc' });
-  const populatedEntries = await Entry.populate(entries, [
-    { path: 'paymentMethod' },
-    { path: 'category' }
-  ]);
-  res.json({ entries: populatedEntries });
-});
+router.get(
+  '/api/entries/:year/:month',
+  jwtMiddleware,
+  catchErrors(async (req, res) => {
+    const { year, month } = req.params;
+    const entries = await Entry.aggregate([
+      { $match: { user: mongoose.Types.ObjectId(req.user._id) } },
+      { $match: { year: Number(year), month: Number(month) } }
+    ]).sort({ date: 'desc' });
+    const populatedEntries = await Entry.populate(entries, [
+      { path: 'paymentMethod' },
+      { path: 'category' }
+    ]);
+    res.json({ entries: populatedEntries });
+  })
+);
 
 // get single
-router.get('/api/entries/:id', jwtMiddleware, async (req, res) => {
-  const [entry] = await Entry.find({ _id: req.params.id, user: req.user })
-    .populate('paymentMethod')
-    .populate('category');
-  res.json({ entry });
-});
-router.get('/api/categories/:id', jwtMiddleware, async (req, res) => {
-  const [category] = await Category.find({ _id: req.params.id, user: req.user });
-  res.json({ category });
-});
-router.get('/api/payment-methods/:id', jwtMiddleware, async (req, res) => {
-  const [paymentMethod] = await PaymentMethod.find({ _id: req.params.id, user: req.user });
-  res.json({ paymentMethod });
-});
+router.get(
+  '/api/entries/:id',
+  jwtMiddleware,
+  catchErrors(async (req, res) => {
+    console.log('passin here', req.params.id);
+    const [entry] = await Entry.find({ _id: req.params.id, user: req.user })
+      .populate('paymentMethod')
+      .populate('category');
+    res.json({ entry });
+  })
+);
+router.get(
+  '/api/categories/:id',
+  jwtMiddleware,
+  catchErrors(async (req, res) => {
+    const [category] = await Category.find({ _id: req.params.id, user: req.user });
+    res.json({ category });
+  })
+);
+router.get(
+  '/api/payment-methods/:id',
+  jwtMiddleware,
+  catchErrors(async (req, res) => {
+    const [paymentMethod] = await PaymentMethod.find({ _id: req.params.id, user: req.user });
+    res.json({ paymentMethod });
+  })
+);
 
 // create
-router.post('/api/entries', jwtMiddleware, async (req, res) => {
-  const [year, month, day] = req.body.date.split('-');
-  const newEntry = {
-    ...req.body,
-    year,
-    month,
-    day
-  };
-  const entry = await Entry.create({ ...newEntry, user: req.user });
-  res.json({ entry });
-});
-router.post('/api/categories', jwtMiddleware, async (req, res) => {
-  const category = await Category.create({ name: req.body.name, user: req.user });
-  res.json({ category });
-});
-router.post('/api/payment-methods', jwtMiddleware, async (req, res) => {
-  const paymentMethod = await PaymentMethod.create({ name: req.body.name, user: req.user });
-  res.json({ paymentMethod });
-});
+router.post(
+  '/api/entries',
+  jwtMiddleware,
+  catchErrors(async (req, res) => {
+    const [year, month, day] = req.body.date.split('-');
+    const newEntry = {
+      ...req.body,
+      year,
+      month,
+      day
+    };
+    const entry = await Entry.create({ ...newEntry, user: req.user });
+    res.json({ entry });
+  })
+);
+router.post(
+  '/api/categories',
+  jwtMiddleware,
+  catchErrors(async (req, res) => {
+    const category = await Category.create({ name: req.body.name, user: req.user });
+    res.json({ category });
+  })
+);
+router.post(
+  '/api/payment-methods',
+  jwtMiddleware,
+  catchErrors(async (req, res) => {
+    const paymentMethod = await PaymentMethod.create({ name: req.body.name, user: req.user });
+    res.json({ paymentMethod });
+  })
+);
 
 // update
-router.put('/api/entries/:id', jwtMiddleware, async (req, res) => {
-  const [year, month, day] = req.body.date.split('-');
-  const updatedEntry = {
-    ...req.body,
-    year,
-    month,
-    day
-  };
-  const entry = await Entry.findOneAndUpdate(
-    { _id: req.params.id, user: req.user },
-    { ...updatedEntry, user: req.user },
-    { new: true }
-  );
-  res.json({ entry });
-});
-router.put('/api/categories/:id', jwtMiddleware, async (req, res) => {
-  const category = await Category.findOneAndUpdate(
-    { _id: req.params.id, user: req.user },
-    { name: req.body.name, user: req.user },
-    { new: true }
-  );
-  res.json({ category });
-});
-router.put('/api/payment-methods/:id', jwtMiddleware, async (req, res) => {
-  const paymentMethod = await PaymentMethod.findOneAndUpdate(
-    { _id: req.params.id, user: req.user },
-    { name: req.body.name, user: req.user },
-    { new: true }
-  );
-  res.json({ paymentMethod });
-});
+router.put(
+  '/api/entries/:id',
+  jwtMiddleware,
+  catchErrors(async (req, res) => {
+    const [year, month, day] = req.body.date.split('-');
+    const updatedEntry = {
+      ...req.body,
+      year,
+      month,
+      day
+    };
+    const entry = await Entry.findOneAndUpdate(
+      { _id: req.params.id, user: req.user },
+      { ...updatedEntry, user: req.user },
+      { new: true }
+    );
+    res.json({ entry });
+  })
+);
+router.put(
+  '/api/categories/:id',
+  jwtMiddleware,
+  catchErrors(async (req, res) => {
+    const category = await Category.findOneAndUpdate(
+      { _id: req.params.id, user: req.user },
+      { name: req.body.name, user: req.user },
+      { new: true }
+    );
+    res.json({ category });
+  })
+);
+router.put(
+  '/api/payment-methods/:id',
+  jwtMiddleware,
+  catchErrors(async (req, res) => {
+    const paymentMethod = await PaymentMethod.findOneAndUpdate(
+      { _id: req.params.id, user: req.user },
+      { name: req.body.name, user: req.user },
+      { new: true }
+    );
+    res.json({ paymentMethod });
+  })
+);
 
 // delete
-router.delete('/api/entries/:id', jwtMiddleware, async (req, res) => {
-  await Entry.findOneAndDelete({ _id: req.params.id, user: req.user });
-  res.sendStatus(200);
-});
-router.delete('/api/categories/:id', jwtMiddleware, async (req, res) => {
-  await Category.findOneAndDelete({ _id: req.params.id, user: req.user });
-  res.sendStatus(200);
-});
-router.delete('/api/payment-methods/:id', jwtMiddleware, async (req, res) => {
-  await PaymentMethod.findOneAndDelete({ _id: req.params.id, user: req.user });
-  res.sendStatus(200);
-});
+router.delete(
+  '/api/entries/:id',
+  jwtMiddleware,
+  catchErrors(async (req, res) => {
+    await Entry.findOneAndDelete({ _id: req.params.id, user: req.user });
+    res.sendStatus(200);
+  })
+);
+router.delete(
+  '/api/categories/:id',
+  jwtMiddleware,
+  catchErrors(async (req, res) => {
+    await Category.findOneAndDelete({ _id: req.params.id, user: req.user });
+    res.sendStatus(200);
+  })
+);
+router.delete(
+  '/api/payment-methods/:id',
+  jwtMiddleware,
+  catchErrors(async (req, res) => {
+    await PaymentMethod.findOneAndDelete({ _id: req.params.id, user: req.user });
+    res.sendStatus(200);
+  })
+);
 
-router.get('/api/menu', jwtMiddleware, async (req, res) => {
-  const menu = await Entry.aggregate([
-    { $match: { user: mongoose.Types.ObjectId(req.user._id) } },
-    { $group: { _id: '$date' } },
-    {
-      $project: {
-        year: { $dateToString: { format: '%Y', date: '$_id' } },
-        month: { $dateToString: { format: '%m', date: '$_id' } }
-      }
-    },
-    {
-      $group: {
-        _id: { year: '$year', month: '$month' }
-      }
-    },
-    {
-      $group: {
-        _id: '$_id.year',
-        months: {
-          $push: {
-            month: '$_id.month'
+router.get(
+  '/api/menu',
+  jwtMiddleware,
+  catchErrors(async (req, res) => {
+    const menu = await Entry.aggregate([
+      { $match: { user: mongoose.Types.ObjectId(req.user._id) } },
+      { $group: { _id: '$date' } },
+      {
+        $project: {
+          year: { $dateToString: { format: '%Y', date: '$_id' } },
+          month: { $dateToString: { format: '%m', date: '$_id' } }
+        }
+      },
+      {
+        $group: {
+          _id: { year: '$year', month: '$month' }
+        }
+      },
+      {
+        $group: {
+          _id: '$_id.year',
+          months: {
+            $push: {
+              month: '$_id.month'
+            }
           }
         }
       }
-    }
-  ]).sort({ _id: 'desc' });
-  res.json({ menu });
-});
+    ]).sort({ _id: 'desc' });
+    res.json({ menu });
+  })
+);
 
 module.exports = router;
